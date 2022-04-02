56e2e92147925538cb25f61b9da2bb46800453b8
GabrielMarmore <gabrielpiraja@hotmail.com>
2022-04-01 22:14:35 -0300

    Feature/home.component.html

--- src/app/tasks.service.ts
+++ src/app/tasks.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
 
+interface Task {
+  name: string;
+  weekday: string;
+  hour: number;
+}
 @Injectable()
 export class TasksService {
+  list: Array<Task> = [];
+  constructor() {}
 
-  constructor() { }
+  getList() {
+    return this.list;
+  }
 
+  add(name: string, weekday: string, hour: number) {
+    this.list.push({ name, weekday, hour });
+  }
+
+  remove(index: number) {
+    this.list.slice(index, 1);
+  }
 }
